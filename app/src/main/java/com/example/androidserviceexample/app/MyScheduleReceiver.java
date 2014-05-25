3361d777e4830f8a6c438f10aecaca8a02ba73bb
josh <howland_josh@yahoo.com>
2014-05-25 14:11:58 -0700

    Added files.  First commit

---
+++ app/src/main/java/com/example/androidserviceexample/app/MyScheduleReceiver.java
@@ -0,0 +1,38 @@
+package com.example.androidserviceexample.app;
+
+/**
+ * Created by josh on 5/25/14.
+ */
+import java.util.Calendar;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MyScheduleReceiver extends BroadcastReceiver {
+
+    // restart service every 1 second
+    private static final long REPEAT_TIME = 1000 * 1;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        AlarmManager service = (AlarmManager) context
+                .getSystemService(Context.ALARM_SERVICE);
+        Intent i = new Intent(context, MyStartServiceReceiver.class);
+        PendingIntent pending = PendingIntent.getBroadcast(context, 0, i,
+                PendingIntent.FLAG_CANCEL_CURRENT);
+        Calendar cal = Calendar.getInstance();
+        // start 30 seconds after boot completed
+        cal.add(Calendar.SECOND, 20);
+        // fetch every 30 seconds
+        // InexactRepeating allows Android to optimize the energy consumption
+        service.setInexactRepeating(AlarmManager.RTC_WAKEUP,
+                cal.getTimeInMillis(), REPEAT_TIME, pending);
+
+        // service.setRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(),
+        // REPEAT_TIME, pending);
+
+    }
+}
---
+++ app/src/main/java/com/example/androidserviceexample/app/MyStartServiceReceiver.java
@@ -0,0 +1,17 @@
+package com.example.androidserviceexample.app;
+
+/**
+ * Created by josh on 5/25/14.
+ */
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MyStartServiceReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent service = new Intent(context, LocalWordService.class);
+        context.startService(service);
+    }
+}
